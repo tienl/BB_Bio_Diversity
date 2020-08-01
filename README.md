3ac27073fe098742cea94feb6138687bac3fcf34
tienl <tien.lee@gmail.com>
2020-08-01 01:27:53 -0700

    challenge done

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# BB_Bio_Diversity
+# Belly Button's Bio Diversity
+
+### In this repo I demonstrate the techniques using HTML, CSS, Javascript, and Plotly (a javascript plotting library), working with data set on belly button and the bacteria found on individuals.  The end result is a webpage published for user to pick and choose samples of data to see multiple data dimensions of the data.  The data source was presented in an anonymized format with metadata of the user, and detailed data of his or her bacteria.  
+
+
+
+### Objective:
+- use javascript to filter only top 10 bacterial species in a bar chart as selected
+- show demographics of the selected
+- show bubble chart for each sample of otu bacteria as selected
+- default load user id 940
+- display gauge chart on wash frequency of selected
+
+### Afterthoughts:
+I've discovered some interesting datapoints once the webpage is built.  It allowed me to quickly review data in different areas on several individuals.  Because of this, I was able to have a better view of the data.  The bubble chart while useful, should probably be updated to use different axis and dimension.  It was done according to coursework requirement.  Additionally, while plotly library is an easy to use library, I did find some aesthetics details difficult to overcome.  Lastly, I've discovered javascript's asynchronous nature during coding, the code probably can be optimized in many ways.  
--- plots.js
+++ plots.js
@@ -1,9 +1,7 @@
 function init() {
-  var selector = d3.select("#selDataset");
-
+  let selector = d3.select("#selDataset");
   d3.json("samples.json").then((data) => {
-    console.log(data);
-    var sampleNames = data.names;
+    let sampleNames = data.names;
     sampleNames.forEach((sample) => {
       selector
         .append("option")
@@ -12,29 +10,107 @@ function init() {
     });
 })}
 
+
 function optionChanged(newSample) {
   buildMetadata(newSample);
   buildCharts(newSample);
 }
 
-
-
 function buildMetadata(sample) {
   d3.json("samples.json").then((data) => {
-    var metadata = data.metadata;
-    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-    var result = resultArray[0];
-    var PANEL = d3.select("#sample-metadata");
-
+    let metadata = data.metadata;
+    let resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    let resultM = resultArray[0];
+    let PANEL = d3.select("#sample-metadata");
     PANEL.html("");
-    PANEL.append("h6").text('Age: ' +result.age);
-    PANEL.append("h6").text('B.Type: ' +result.btype);
-    PANEL.append("h6").text('Ethnicity: ' +result.ethnicity);
-    PANEL.append("h6").text('Gender: ' +result.gender);
-    PANEL.append("h6").text('ID: ' +result.id);
-    PANEL.append("h6").text('Location: ' +result.location);
-    PANEL.append("h6").text('W.Freq: ' +result.wfreq);
+    PANEL.append("h6").text('Age: ' +resultM.age);
+    PANEL.append("h6").text('BBType: ' +resultM.bbtype);
+    PANEL.append("h6").text('Ethnicity: ' +resultM.ethnicity);
+    PANEL.append("h6").text('Gender: ' +resultM.gender);
+    PANEL.append("h6").text('ID: ' +resultM.id);
+    PANEL.append("h6").text('Location: ' +resultM.location);
+    PANEL.append("h6").text('W.Freq: ' +resultM.wfreq);
   });
 }
 
+function buildCharts(sample) {
+  d3.json("samples.json").then((data) => {
+    let samples = data.samples;
+    let resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+    let result = resultArray[0];
+    let otuLabels = result.otu_labels.slice(0,10);
+    let xaxis = result.sample_values.slice(0,10);
+    let ym = result.otu_ids.slice(0,10);
+    let yaxis = ym.map((item) => {
+      return ('OTU ' + item);
+    });
+
+    let trace1 = {
+      x: xaxis,
+      y: yaxis,
+      type: "bar",
+      orientation: 'h',
+      text: otuLabels,
+      marker: {
+        color: ym,
+        colorscale: 'Jet'
+        },
+      //descending on the bar graph
+      transforms: [{
+        type: 'sort',
+        target: 'y',
+        order: 'descending'
+      }]
+    };
+    let data1 = [trace1];
+    let layout = {
+        title: "Individual's top 10 Bacterial Species",
+        xaxis: { title: "Count"},
+        yaxis: { title: "Bacteria ID"}
+      };
+      Plotly.newPlot("bar", data1, layout);
+
+    //bubble chart below
+    let trace2 = {
+      x: ym,
+      y: xaxis,
+      mode: 'markers',
+      marker: {
+        size: xaxis,
+        color: ym,
+        colorscale: 'Jet'
+        },
+      text: otuLabels,
+      type: 'scatter'
+    };
+    let data2 = [trace2];
+    let layout2 = {
+      title: "Individual's top 10 Bacterial Species",
+      xaxis: { title: "OTU ID"},
+      yaxis: { title: "Sample Value"}
+    };
+    Plotly.newPlot("bubble", data2, layout2);
+
+    // gauge chart below
+    d3.json("samples.json").then((data) => {
+       let metadata = data.metadata;
+       let resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+       let result = resultArray[0];
+       let wash = result.wfreq;
+       let trace3 = [{
+      		domain: { x: [0, 1], y: [0, 1] },
+      		value: wash,
+      		title: { text: "Weekly Wash Frequency" },
+      		type: "indicator",
+      		mode: "gauge+number"
+       }];
+       let layout3 = {width: 600, height: 500, margin: { t: 0, b: 0 } };
+       Plotly.newPlot('gauge', trace3, layout3);
+    });
+  });
+}
+
+
 init();
+//loading script call 940 as default
+optionChanged(940);
